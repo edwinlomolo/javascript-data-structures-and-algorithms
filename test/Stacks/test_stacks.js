f7403aaa54c8537aae52eb60564655a3bd49d3c0
3dw1nM0535 <workockmoses@gmail.com>
2018-12-08 20:33:59 +0300

    chore(stack test cases)

--- test/Stacks/test_stacks.js
+++ test/Stacks/test_stacks.js
@@ -3,10 +3,20 @@
 const Stack = require("../../Stacks/stack");
 const expect = require("expect.js");
 
+let stack;
+
 describe("Interacting with stack data structure", () => {
   it("Should be able to create a new stack", () => {
-    const stack = new Stack();
+    stack = new Stack();
     expect(stack.isEmpty()).to.be(true);
     expect(stack.size()).to.be.equal(0);
+  });
+  it("Should be able to add elements to the stack", () => {
+    stack.push(0);
+    stack.push(1);
+    stack.push(2);
+    expect(stack.isEmpty()).to.be(false);
+    expect(stack.size()).not.to.equal(0);
+    expect(stack.size()).to.be.equal(3);
   });
 });
