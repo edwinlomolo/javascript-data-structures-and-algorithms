2d3e9c5f4f9791762356d2d5ef236e5892542760
3dw1nM0535 <workockmoses@gmail.com>
2018-12-08 20:27:45 +0300

    chore(stack test cases)

---
+++ test/Stacks/test_stacks.js
@@ -0,0 +1,12 @@
+// Stack data structure test drive
+
+const Stack = require("../Stacks/stack");
+const expect = require("expect.js");
+
+describe("Interacting with stack data structure", () => {
+  it("Should be able to create a new stack", () => {
+    const stack = new Stack();
+    expect(stack.isEmpty()).to.be(true);
+    expect(stack.size()).to.be.equal(0);
+  });
+});
