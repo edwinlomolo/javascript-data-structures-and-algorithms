66d15cb19f718213fb6eab626de08c0987eed875
3dw1nM0535 <workockmoses@gmail.com>
2019-03-07 16:27:19 +0300

    chore(selection sort algorithm test cases)

--- test/Sorting Algorithms/test_sorting_algorithms.js
+++ test/Sorting Algorithms/test_sorting_algorithms.js
@@ -6,14 +6,19 @@ const expect = require("expect.js");
 
 const items = [12, 10, 23, 40];
 const expected = [10, 12, 23, 40];
+let sortedArray;
 
 describe("Implementing sorting algorithms", () => {
   it("Should be able to perform a sort using bubble sort algorithm", () => {
-    const sortedArray = bubbleSort(items);
+    sortedArray = bubbleSort(items);
     for (let i = 0; i < sortedArray.length; i += 1) {
       expect(expected[i]).to.be.equal(sortedArray[i]);
     }
   });
   it("Should be able to perform a sort using selection sort algorithm", () => {
-  })
+    sortedArray = selectionSort(items);
+    for (let i = 0; i < sortedArray.length; i += 1) {
+      expect(expected[i]).to.be.equal(sortedArray[i]);
+    }
+  });
 });
