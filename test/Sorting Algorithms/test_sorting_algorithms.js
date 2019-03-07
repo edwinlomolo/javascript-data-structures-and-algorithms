c096d2c56cb906813dec91f6ce1a63221845b0d3
3dw1nM0535 <workockmoses@gmail.com>
2019-03-07 17:06:23 +0300

    chore(insertion sort algorithm implementation)

--- test/Sorting Algorithms/test_sorting_algorithms.js
+++ test/Sorting Algorithms/test_sorting_algorithms.js
@@ -2,6 +2,7 @@
 
 const bubbleSort = require("../../Sorting and Searching algorithms/bubble_sort");
 const selectionSort = require("../../Sorting and Searching algorithms/selection_sort");
+const insertionSort = require("../../Sorting and Searching algorithms/insertion_sort");
 const expect = require("expect.js");
 
 const items = [12, 10, 23, 40, 40, 42, 2, 9];
@@ -17,6 +18,12 @@ describe("Implementing sorting algorithms", () => {
   });
   it("Should be able to perform a sort using selection sort algorithm", () => {
     sortedArray = selectionSort(items);
+    for (let i = 0; i < sortedArray.length; i += 1) {
+      expect(expected[i]).to.be.equal(sortedArray[i]);
+    }
+  });
+  it("Should be able to perform a sort using insertion sort algorithm", () => {
+    sortedArray = insertionSort(items);
     for (let i = 0; i < sortedArray.length; i += 1) {
       expect(expected[i]).to.be.equal(sortedArray[i]);
     }
