09b07eacd20d99d610804b9e0f1a81aaf6ccda03
3dw1nM0535 <workockmoses@gmail.com>
2019-03-31 18:57:55 +0300

    chore(implement test cases for merge sort)

--- test/Sorting Algorithms/test_sorting_algorithms.js
+++ test/Sorting Algorithms/test_sorting_algorithms.js
@@ -3,11 +3,17 @@
 const bubbleSort = require("../../Sorting and Searching algorithms/bubble_sort");
 const selectionSort = require("../../Sorting and Searching algorithms/selection_sort");
 const insertionSort = require("../../Sorting and Searching algorithms/insertion_sort");
+const mergeSort = require("../../Sorting and Searching algorithms/merge_sort");
 const expect = require("expect.js");
 
 const items = [12, 10, 23, 40, 40, 42, 2, 9];
 const expected = [2, 9, 10, 12, 23, 40, 40, 42];
 let sortedArray;
+
+// Hook
+beforeEach(() => {
+  sortedArray = [];
+});
 
 describe("Implementing sorting algorithms", () => {
   it("Should be able to perform a sort using bubble sort algorithm", () => {
@@ -24,6 +30,12 @@ describe("Implementing sorting algorithms", () => {
   });
   it("Should be able to perform a sort using insertion sort algorithm", () => {
     sortedArray = insertionSort(items);
+    for (let i = 0; i < sortedArray.length; i += 1) {
+      expect(expected[i]).to.be.equal(sortedArray[i]);
+    }
+  });
+  it("Should be able to perform a sort using merge sort algorithm", () => {
+    sortedArray = mergeSort(items);
     for (let i = 0; i < sortedArray.length; i += 1) {
       expect(expected[i]).to.be.equal(sortedArray[i]);
     }
