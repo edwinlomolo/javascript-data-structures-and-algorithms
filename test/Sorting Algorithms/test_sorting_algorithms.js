954598cd498becb03f6f69bfe3ce517fbfb2e247
3dw1nM0535 <workockmoses@gmail.com>
2019-04-05 11:43:32 +0300

    chore(quick sort algorithm implementation)

--- test/Sorting Algorithms/test_sorting_algorithms.js
+++ test/Sorting Algorithms/test_sorting_algorithms.js
@@ -4,6 +4,7 @@ const bubbleSort = require("../../Sorting and Searching algorithms/bubble_sort")
 const selectionSort = require("../../Sorting and Searching algorithms/selection_sort");
 const insertionSort = require("../../Sorting and Searching algorithms/insertion_sort");
 const mergeSort = require("../../Sorting and Searching algorithms/merge_sort");
+const quickSort = require("../../Sorting and Searching algorithms/quickSort");
 const expect = require("expect.js");
 
 const items = [12, 10, 23, 40, 40, 42, 2, 9];
@@ -16,26 +17,37 @@ beforeEach(() => {
 });
 
 describe("Implementing sorting algorithms", () => {
+  // Bubble sort algorithm implementation
   it("Should be able to perform a sort using bubble sort algorithm", () => {
     sortedArray = bubbleSort(items);
     for (let i = 0; i < sortedArray.length; i += 1) {
       expect(expected[i]).to.be.equal(sortedArray[i]);
     }
   });
+  // Selection sort algorithm implementation
   it("Should be able to perform a sort using selection sort algorithm", () => {
     sortedArray = selectionSort(items);
     for (let i = 0; i < sortedArray.length; i += 1) {
       expect(expected[i]).to.be.equal(sortedArray[i]);
     }
   });
+  // Insertion sort algorithm implementation
   it("Should be able to perform a sort using insertion sort algorithm", () => {
     sortedArray = insertionSort(items);
     for (let i = 0; i < sortedArray.length; i += 1) {
       expect(expected[i]).to.be.equal(sortedArray[i]);
     }
   });
+  // Merge sort algorithm implementation
   it("Should be able to perform a sort using merge sort algorithm", () => {
     sortedArray = mergeSort(items);
+    for (let i = 0; i < sortedArray.length; i += 1) {
+      expect(expected[i]).to.be.equal(sortedArray[i]);
+    }
+  });
+  // Quick sort algorithm implementation
+  it("Should be able to perform a sort using quick sort algorithm", () => {
+    sortedArray = quickSort(items);
     for (let i = 0; i < sortedArray.length; i += 1) {
       expect(expected[i]).to.be.equal(sortedArray[i]);
     }
