be2e67b2350937894742febfb7c4d85b16f6ad8e
3dw1nM0535 <workockmoses@gmail.com>
2019-04-05 13:16:00 +0300

    chore(heap sort algorithm implementation)

--- test/Sorting Algorithms/test_sorting_algorithms.js
+++ test/Sorting Algorithms/test_sorting_algorithms.js
@@ -5,6 +5,7 @@ const selectionSort = require("../../Sorting and Searching algorithms/selection_
 const insertionSort = require("../../Sorting and Searching algorithms/insertion_sort");
 const mergeSort = require("../../Sorting and Searching algorithms/merge_sort");
 const quickSort = require("../../Sorting and Searching algorithms/quickSort");
+const heapSort = require("../../Sorting and Searching algorithms/heap_sort");
 const expect = require("expect.js");
 
 const items = [12, 10, 23, 40, 40, 42, 2, 9];
@@ -48,6 +49,12 @@ describe("Implementing sorting algorithms", () => {
   // Quick sort algorithm implementation
   it("Should be able to perform a sort using quick sort algorithm", () => {
     sortedArray = quickSort(items);
+    for (let i = 0; i < sortedArray.length; i += 1) {
+      expect(expected[i]).to.be.equal(sortedArray[i]);
+    }
+  });
+  it("Should be able to perform a sort using heap sort algorithm", () => {
+    sortedArray = heapSort(items);
     for (let i = 0; i < sortedArray.length; i += 1) {
       expect(expected[i]).to.be.equal(sortedArray[i]);
     }
