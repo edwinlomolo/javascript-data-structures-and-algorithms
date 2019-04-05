738c9b96a49362ff9747692d9adbe93111b969bc
3dw1nM0535 <workockmoses@gmail.com>
2019-04-05 14:36:05 +0300

    chore(test cases for binary search)

--- test/Search algorithms/test_searching.js
+++ test/Search algorithms/test_searching.js
@@ -1,6 +1,7 @@
 // Searching algorithms implementation using test cases
 const expect = require("expect.js");
 const sequentialSearch = require("../../Searching algorithms/sequential_search");
+const binarySearch = require("../../Searching algorithms/binary_search");
 
 const data = [23, 12, 4, 42];
 
@@ -16,6 +17,14 @@ describe("Interacting with searching algorithms", () => {
   });
   it("Should be able to handle a search for an item that is not in the list", () => {
     index = sequentialSearch(40, data);
-    expect(data[index]).to.be.equal(undefined);
+    expect(data[index]).to.be(undefined);
+  });
+  it("Should be able to perform a search using the binary search algorithm", () => {
+    index = binarySearch(42, data);
+    expect(data[index]).to.be.equal(42);
+  });
+  it("Should be able to handle a search for an item that isn't in the list", () => {
+    index = binarySearch(400, data);
+    expect(data[index]).to.be(undefined);
   });
 });
