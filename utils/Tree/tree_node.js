fb7c8395aa20b39169c935601e2b077c042605b0
3dw1nM0535 <workockmoses@gmail.com>
2019-04-12 17:21:51 +0300

    feat(learn binary tree data structure)

--- utils/Tree/tree_node.js
+++ utils/Tree/tree_node.js
@@ -1,4 +1,4 @@
-// Node class helper for tree data structure definition and implementation
+// Node class helper for tree data structure definition
 
 class Node {
   constructor(key) {
@@ -8,4 +8,49 @@ class Node {
   }
 }
 
-module.exports = Node;
+// Insert key helper function definition
+const insertNode = (node, newNode) => {
+  if (newNode.key < node.key) {
+    if (node.left === null) {
+      node.left = newNode;
+    }
+    insertNode(node.left, newNode);
+  }
+  if (node.right === null) {
+    node.right = newNode;
+  }
+  insertNode(node.right, newNode);
+};
+
+// inOrderTraverseNode helper function
+const inOrderTraverseNode = (node, cb) => {
+  if (node !== null) {
+    inOrderTraverseNode(node.left, cb);
+    cb(node.key);
+    inOrderTraverseNode(node.right, cb);
+  }
+};
+
+const preOrderTraverseNode = (node, cb) => {
+  if (node !== null) {
+    cb(node.key);
+    preOrderTraverseNode(node.left, cb);
+    preOrderTraverseNode(node.right, cb);
+  }
+};
+
+const postOrderTraverseNode = (node, cb) => {
+  if (node !== null) {
+    postOrderTraverseNode(node.left, cb);
+    postOrderTraverseNode(node.right, cb);
+    cb(node.key);
+  }
+};
+
+module.exports = {
+  Node,
+  insertNode,
+  inOrderTraverseNode,
+  preOrderTraverseNode,
+  postOrderTraverseNode,
+};
