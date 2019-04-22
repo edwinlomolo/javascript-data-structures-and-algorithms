693b6e8ed45df297e8e405f6bc08ae00d49c64bc
3dw1nM0535 <workockmoses@gmail.com>
2019-04-22 12:10:21 +0300

    chore(graph ds test cases)

--- test/Graphs/test_graph_ds_.js
+++ test/Graphs/test_graph_ds_.js
@@ -7,7 +7,7 @@ const Graph = require("../../Graphs/graph");
 let graph;
 
 // Hook
-beforeEach(() => {
+before(() => {
   graph = new Graph();
 });
 
@@ -15,5 +15,22 @@ describe("Interacting with Graph data structure", () => {
   it("Should be able to initialize a new Graph data structure", () => {
     expect(graph.vertices.length).to.be.equal(0);
     expect(Object.keys(graph.adjList.items).length).to.be.equal(0);
+  });
+  it("Should be able to add a vertex to the Graph", () => {
+    const vertices = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+    for (let i = 0; i < vertices.length; i += 1) {
+      graph.addVertex(vertices[i]);
+    }
+    expect(graph.vertices.length).to.be.equal(9);
+    graph.addEdge('A', 'B');
+    graph.addEdge('A', 'C');
+    graph.addEdge('A', 'D');
+    graph.addEdge('C', 'D');
+    graph.addEdge('C', 'G');
+    graph.addEdge('D', 'G');
+    graph.addEdge('D', 'H');
+    graph.addEdge('B', 'E');
+    graph.addEdge('B', 'F');
+    graph.addEdge('E', 'I');
   });
 });
