a6f01ad5f83a341859a5fbd6cf1047548884cb4d
3dw1nM0535 <workockmoses@gmail.com>
2019-04-22 11:36:32 +0300

    feat(learn graph ds)

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -7,9 +7,23 @@ class Graph {
     this.adjList = new Dictionary();
   }
 
+  /**
+   * add a vertex to a list of vertices
+   * @param  v vertex to be added to the graph
+   */
   addVertex(v) {
     this.vertices.push(v);
     this.adjList.set(v, []);
+  }
+
+  /**
+   * this method receives two vertices as parameters.
+   * First, we add an edge from vertex v to vertex w
+   * by adding w to the adjacent list of vertex v.
+   */
+  addEdge(v, w) {
+    this.adjList.get(v).push(w);
+    this.adjList.get(w).push(v);
   }
 }
 
